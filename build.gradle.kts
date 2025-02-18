703ef0fdfeac10dbf5f94185cead35478e82e0ef
Viacheslav Suvorov <viacheslav.suvorov@jetbrains.com>
2025-02-18 13:58:42 +0100

    Add opentelemetry

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 group = "com.jetbrains"
 version = "1.0-SNAPSHOT"
 val ktor_version: String by project
+val opentelemetry_version: String by project
 val logback_version: String by project
 val mlflow_client_version: String by project
 
@@ -18,12 +19,14 @@ dependencies {
     implementation("com.google.inject:guice:7.0.0")
     implementation("com.openai:openai-java:0.21.1")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("org.mlflow:mlflow-client:2.20.1")
     implementation("io.ktor:ktor-client-core:$ktor_version")
     implementation("io.ktor:ktor-client-cio:$ktor_version")
     implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
     implementation("org.mlflow:mlflow-client:$mlflow_client_version")
+    implementation("io.opentelemetry:opentelemetry-api:$opentelemetry_version")
+    implementation("io.opentelemetry:opentelemetry-sdk:$opentelemetry_version")
+    implementation("io.opentelemetry:opentelemetry-exporter-logging:$opentelemetry_version")
     implementation("org.yaml:snakeyaml:2.3")
 
     testImplementation(kotlin("test"))
--- src/main/kotlin/ai/mlflow/fluent/Example.kt
+++ src/main/kotlin/ai/mlflow/fluent/Example.kt
@@ -1,17 +1,82 @@
 package org.example.ai.mlflow.fluent
+
 import com.google.inject.Guice
 import com.google.inject.Injector
+import io.opentelemetry.api.GlobalOpenTelemetry
+import io.opentelemetry.sdk.OpenTelemetrySdk
+import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor
+import io.opentelemetry.sdk.common.CompletableResultCode
+import io.opentelemetry.sdk.trace.SdkTracerProvider
+import io.opentelemetry.sdk.trace.data.SpanData
+import io.opentelemetry.sdk.trace.export.SpanExporter
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+class ParentChildLoggingSpanExporter : SpanExporter {
+    override fun export(spans: MutableCollection<SpanData>): CompletableResultCode {
+        for (span in spans) {
+            println(
+                "Span: ${span.name}, Span ID: ${span.spanId}, Parent Span ID: ${span.parentSpanId}, Trace ID: ${span.traceId}"
+            )
+        }
+        return CompletableResultCode.ofSuccess()
+    }
+
+    override fun shutdown(): CompletableResultCode {
+        return CompletableResultCode.ofSuccess()
+    }
+
+    override fun flush(): CompletableResultCode {
+        return CompletableResultCode.ofSuccess()
+    }
+}
+
+fun setupTracing() {
+    val tracerProvider = SdkTracerProvider.builder()
+        .addSpanProcessor(SimpleSpanProcessor.create(ParentChildLoggingSpanExporter()))
+        .build()
+
+    GlobalOpenTelemetry.set(
+        OpenTelemetrySdk.builder()
+            .setTracerProvider(tracerProvider)
+            .build()
+    )
+}
 
 open class MyClass {
     @KotlinFlowTrace
     open fun someFunction(x: Int, y: Int, z: Int = 2): Int {
-        Thread.sleep(5000)
-        return x + (y - z)
+        Thread.sleep(500)
+        return a(x) + b(y - z)
+    }
+
+    @KotlinFlowTrace
+    open fun a(x: Int): Int {
+        Thread.sleep(200)
+        return x - 1
+    }
+
+    @KotlinFlowTrace
+    open fun b(x: Int): Int {
+        Thread.sleep(200)
+        return x + 1
     }
 }
 
 fun main() {
-    val injector: Injector = Guice.createInjector(KotlinFlowTraceModule())
-    val myClass = injector.getInstance(MyClass::class.java)
-    println(myClass.someFunction(2, 4))
+    runBlocking {
+        coroutineScope {
+            setupTracing()
+            val injector: Injector = Guice.createInjector(KotlinFlowTraceModule())
+            val myClass = injector.getInstance(MyClass::class.java)
+
+            launch {
+                myClass.someFunction(2, 4)
+            }
+            launch {
+                myClass.someFunction(3, 5)
+            }
+        }
+    }
 }
