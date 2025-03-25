f8fbcba00579e2ec28b18c2117fc96d7d256c2cc
Viacheslav Suvorov <suvslavka@gmail.com>
2025-03-25 16:40:24 +0100

    Start

---
+++ src/main/kotlin/ai/mlflow/fluent/test.kt
@@ -0,0 +1,75 @@
+import kotlinx.coroutines.delay
+import net.bytebuddy.agent.ByteBuddyAgent
+import net.bytebuddy.agent.builder.AgentBuilder
+import net.bytebuddy.agent.builder.AgentBuilder.RedefinitionStrategy
+import net.bytebuddy.asm.Advice
+import net.bytebuddy.matcher.ElementMatchers
+import java.lang.instrument.Instrumentation
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.CoroutineContext
+
+@Retention(AnnotationRetention.RUNTIME)
+annotation class TrackExecution2
+
+object ExecutionTimeAgent {
+    @JvmStatic
+    fun premain(agentArgs: String?, inst: Instrumentation) {
+        AgentBuilder.Default()
+            .with(RedefinitionStrategy.RETRANSFORMATION)
+            .with(RedefinitionStrategy.Listener.StreamWriting.toSystemError())
+            .type(ElementMatchers.nameEndsWith("TargetClass"))
+            .transform { builder, _, _, _, _ ->
+                builder.visit(Advice.to(Demo.Advice1::class.java).on(ElementMatchers.isAnnotatedWith(TrackExecution2::class.java)))
+            }
+            .installOn(inst)
+    }
+}
+
+class TargetClass {
+    @TrackExecution2
+    suspend fun testMethod(s: String, f: String): Int {
+        delay(20)
+        println("In method: $s")
+        return 4
+    }
+}
+
+val argsProcessor: ArgsProcessor = object : ArgsProcessor {
+    override fun process(args: Array<Any?>) {
+        val lastArg = args.lastOrNull()
+        if (lastArg is Continuation<*>) {
+            println(lastArg.context)
+            println("HIHI")
+            args[args.size - 1] = lastArg.withContext(MyContext())
+        }
+        args[0] = "${args[0]}-suffixe"
+        args[1] = "replaced"
+    }
+}
+
+interface ArgsProcessor {
+    fun process(args: Array<Any?>)
+}
+
+
+class MyContext : CoroutineContext.Element {
+    companion object Key : CoroutineContext.Key<MyContext>
+    override val key: CoroutineContext.Key<*> = Key
+}
+
+fun <T> Continuation<T>.withContext(context: CoroutineContext): Continuation<T> {
+    return object : Continuation<T> {
+        override val context: CoroutineContext = this@withContext.context + context
+
+        override fun resumeWith(result: Result<T>) {
+            this@withContext.resumeWith(result)
+        }
+    }
+}
+
+
+suspend fun main() {
+    ExecutionTimeAgent.premain(null, ByteBuddyAgent.install())
+    val target = TargetClass()
+    target.testMethod("Sample Argument", "HIHI")
+}
