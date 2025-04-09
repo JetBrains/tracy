8b31b316255f7f50f4b34f78ba1cc7f8284c7ebd
Viacheslav Suvorov <suvslavka@gmail.com>
2025-04-09 16:24:54 +0200

    JBAI-13053 Support publishing ai-dev-kit on space

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,47 @@
 plugins {
     kotlin("jvm") version "2.1.0"
     kotlin("plugin.serialization") version "2.1.0"
+    `maven-publish`
 }
 
 group = "com.jetbrains"
 version = "1.0-SNAPSHOT"
+
 val ktor_version: String by project
 val opentelemetry_version: String by project
 val logback_version: String by project
 val mlflow_client_version: String by project
 val testcontainers_version: String by project
 
+val space_username: String? by project
+val space_password: String? by project
+
 repositories {
     mavenCentral()
 }
 
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = "ai-dev-kit"
+            version = project.version.toString()
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            url = uri("https://packages.jetbrains.team/maven/p/ai-development-kit/ai-development-kit")
+            credentials {
+                username = System.getenv("SPACE_USERNAME") ?: space_username ?: error("Environment variable 'SPACE_USERNAME' is not set.")
+                password = System.getenv("SPACE_PASSWORD") ?: space_password ?: error("Environment variable 'SPACE_PASSWORD' is not set.")
+            }
+        }
+    }
+}
+
 dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("com.google.inject:guice:7.0.0")
     implementation("com.openai:openai-java:0.34.1")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("io.ktor:ktor-client-core:$ktor_version")
