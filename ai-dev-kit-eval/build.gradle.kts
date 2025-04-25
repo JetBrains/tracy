bb58c0b178c4b400217ecb5e18d75bc0e1eacd03
Nikolai Gruzinov <nikolai.gruzinov@jetbrains.com>
2025-04-25 19:45:09 +0200

    eval submodule shouldn't be multiplatform

--- ai-dev-kit-eval/build.gradle.kts
+++ ai-dev-kit-eval/build.gradle.kts
@@ -1,7 +1,5 @@
 plugins {
-    id("java")
-    alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.kotlin.serialization)
+    kotlin("jvm")
 }
 
 group = "com.jetbrains"
@@ -11,32 +9,11 @@ repositories {
     mavenCentral()
 }
 
-kotlin {
-    jvm {
-        compilerOptions.jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
-        withJava()
-    }
-
-    js(IR) {
-        browser()
-    }
-
-    sourceSets {
-        val commonMain by getting {
-            dependencies {
-                implementation(project(":ai-dev-kit-core"))
-                implementation(libs.kotlin)
-            }
-        }
-    }
+dependencies {
+    implementation(project(":ai-dev-kit-core"))
+    implementation(libs.kotlin)
 }
 
 kotlin {
     jvmToolchain(17)
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    compilerOptions {
-        freeCompilerArgs.add("-Xexpect-actual-classes")
-    }
 }
