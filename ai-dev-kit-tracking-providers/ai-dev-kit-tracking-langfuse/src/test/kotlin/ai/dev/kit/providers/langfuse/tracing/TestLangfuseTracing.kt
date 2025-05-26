aaf37af712607e6a01eff76a1f77ad292a30f707
Nikolai Gruzinov <nikolai.gruzinov@jetbrains.com>
2025-05-26 11:30:24 +0200

    Fix the tests: pass getExperimentId

--- ai-dev-kit-tracking-providers/ai-dev-kit-tracking-langfuse/src/test/kotlin/ai/dev/kit/providers/langfuse/tracing/TestLangfuseTracing.kt
+++ ai-dev-kit-tracking-providers/ai-dev-kit-tracking-langfuse/src/test/kotlin/ai/dev/kit/providers/langfuse/tracing/TestLangfuseTracing.kt
@@ -3,24 +3,27 @@ package ai.dev.kit.providers.langfuse.tracing
 import ai.dev.kit.fluent.TestAutologTracingBase
 import ai.dev.kit.fluent.TestFluentTracingBase
 import ai.dev.kit.fluent.TestSuspendFluentTracingBase
-import ai.dev.kit.providers.langfuse.KotlinLangfuseClient
 import ai.dev.kit.providers.langfuse.getAllTracesForProject
 import org.junit.jupiter.api.Tag
 
+//const val TEST_PROJECT_NAME = "ai-dev-kit-tracing-tests"
+const val TEST_PROJECT_NAME = "test_project_1"
+
+
 @Tag("SkipForNonLocal")
 class TestAutologTracingLangfuse : TestAutologTracingBase(
-    ::getAllTracesForProject,
-    KotlinLangfuseClient
+    getTraces = ::getAllTracesForProject,
+    getExperimentId = { TEST_PROJECT_NAME }
 ), LangfuseTracingTests
 
 @Tag("SkipForNonLocal")
 class TestFluentTracingLangfuse : TestFluentTracingBase(
-    ::getAllTracesForProject,
-    KotlinLangfuseClient
+    getTraces = ::getAllTracesForProject,
+    getExperimentId = { TEST_PROJECT_NAME }
 ), LangfuseTracingTests
 
 @Tag("SkipForNonLocal")
 class TestSuspendFluentTracingLangfuse : TestSuspendFluentTracingBase(
-    ::getAllTracesForProject,
-    KotlinLangfuseClient
+    getTraces = ::getAllTracesForProject,
+    getExperimentId = { TEST_PROJECT_NAME }
 ), LangfuseTracingTests
