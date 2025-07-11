dacb977f98a625c51370a53914167b52b1c0f594
Anton.Bragin <anton.bragin@jetbrains.com>
2025-07-11 11:33:51 +0200

    Functions for Langfuse and Weave span exporter creations added

---
+++ ai-dev-kit-tracing/src/jvmMain/kotlin/ai/dev/kit/exporters/Langfuse.kt
@@ -0,0 +1,39 @@
+package ai.dev.kit.exporters
+
+import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter
+import java.util.Base64
+import java.util.concurrent.TimeUnit
+
+/**
+ * Creates an OpenTelemetry span exporter that sends data to [Langfuse](https://langfuse.com/).
+ *
+ * @param langfuseUrl the base URL of the Langfuse instance. If not set is retrieved from `LANGFUSE_URL` environment variable. Defaults to [https://cloud.langfuse.com](https://cloud.langfuse.com).
+ * @param langfusePublicKey if not set is retrieved from `LANGFUSE_PUBLIC_KEY` environment variable.
+ * @param langfuseSecretKey if not set is retrieved from `LANGFUSE_SECRET_KEY` environment variable.
+ * @param timeout OpenTelemetry SpanExporter timeout in seconds. See [io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporterBuilder.setTimeout].
+ *
+ * @see <a href="https://langfuse.com/docs/get-started#create-new-project-in-langfuse">How to create a new project in Langfuse</a>
+ * @see <a href="https://langfuse.com/faq/all/where-are-langfuse-api-keys">How to set up API keys in Langfuse</a>
+ * @see <a href="https://langfuse.com/docs/opentelemetry/get-started#opentelemetry-endpoint">Langfuse OpenTelemetry Docs</a>
+ */
+fun createLangfuseExporter(
+    langfuseUrl: String? = null,
+    langfusePublicKey: String? = null,
+    langfuseSecretKey: String? = null,
+    timeout: Long = 10,
+): OtlpHttpSpanExporter? {
+    val url = langfuseUrl ?: System.getenv()["LANGFUSE_URL"] ?: "https://cloud.langfuse.com"
+    val publicKey = langfusePublicKey ?: System.getenv()["LANGFUSE_PUBLIC_KEY"]
+    ?: throw IllegalArgumentException("LANGFUSE_PUBLIC_KEY is not set")
+    val secretKey = langfuseSecretKey ?: System.getenv()["LANGFUSE_SECRET_KEY"]
+    ?: throw IllegalArgumentException("LANGFUSE_SECRET_KEY is not set")
+
+    val credentials = "$publicKey:$secretKey"
+    val auth = Base64.getEncoder().encodeToString(credentials.toByteArray(Charsets.UTF_8))
+
+    return OtlpHttpSpanExporter.builder()
+        .setTimeout(timeout, TimeUnit.SECONDS)
+        .setEndpoint("$url/api/public/otel/v1/traces")
+        .addHeader("Authorization", "Basic $auth")
+        .build()
+}
---
+++ ai-dev-kit-tracing/src/jvmMain/kotlin/ai/dev/kit/exporters/Weave.kt
@@ -0,0 +1,39 @@
+package ai.dev.kit.exporters
+
+import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter
+import java.util.Base64
+import java.util.concurrent.TimeUnit
+
+/**
+ * Configure an OpenTelemetry span exporter that sends data to [W&B Weave](https://wandb.ai/site/weave/).
+ *
+ * @param weaveOtelBaseUrl the URL of the Weave OpenTelemetry endpoint. If not set is retrieved from `WEAVE_URL` environment variable. Defaults to [https://trace.wandb.ai](https://trace.wandb.ai).
+ * @param weaveEntity can be found by visiting your W&B dashboard at [https://wandb.ai/home](https://wandb.ai/home) and checking the *Teams* field in the left sidebar.
+ * If not set is retrieved from `WEAVE_ENTITY` environment variable.
+ * @param weaveProjectName name of your Weave project. If not set is retrieved from `WEAVE_PROJECT_NAME` environment variable.
+ * @param weaveApiKey can be created on the [https://wandb.ai/authorize](https://wandb.ai/authorize) page. If not set is retrieved from `WEAVE_API_KEY` environment variable.
+ * @param timeout OpenTelemetry SpanExporter timeout in seconds. See [io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporterBuilder.setTimeout].
+ *
+ * @see <a href="https://weave-docs.wandb.ai/guides/tracking/otel/">Weave OpenTelemetry Docs</a>
+ */
+fun createWeaveExporter(
+    weaveOtelBaseUrl: String? = null,
+    weaveEntity: String? = null,
+    weaveProjectName: String? = null,
+    weaveApiKey: String? = null,
+    timeout: Long = 10,
+): OtlpHttpSpanExporter? {
+    val url = weaveOtelBaseUrl ?: System.getenv()["WEAVE_URL"] ?: "https://trace.wandb.ai"
+    val entity = weaveEntity ?: System.getenv()["WEAVE_ENTITY"] ?: throw IllegalArgumentException("WEAVE_ENTITY is not set")
+    val projectName = weaveProjectName ?: System.getenv()["WEAVE_PROJECT_NAME"] ?: "koog-tracing"
+    val apiKey = weaveApiKey ?:  System.getenv()["WEAVE_API_KEY"] ?: throw IllegalArgumentException("WEAVE_API_KEY is not set")
+
+    val auth = Base64.getEncoder().encodeToString("api:$apiKey".toByteArray(Charsets.UTF_8))
+
+    return OtlpHttpSpanExporter.builder()
+        .setTimeout(timeout, TimeUnit.SECONDS)
+        .setEndpoint("$url/otel/v1/traces")
+        .addHeader("project_id", "$entity/$projectName")
+        .addHeader("Authorization", "Basic $auth")
+        .build()
+}
