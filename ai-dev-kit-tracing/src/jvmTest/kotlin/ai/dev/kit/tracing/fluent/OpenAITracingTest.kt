2adb944ffc496951aba213593d1e48d851e27748
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2025-07-14 18:04:37 +0200

    feat: start writing tests for the instrumentation

--- ai-dev-kit-tracing/src/jvmTest/kotlin/ai/dev/kit/tracing/fluent/OpenAITracingTest.kt
+++ ai-dev-kit-tracing/src/jvmTest/kotlin/ai/dev/kit/tracing/fluent/OpenAITracingTest.kt
@@ -4,12 +4,18 @@ import ai.dev.kit.instrument
 import ai.dev.kit.tracing.BaseOpenTelemetryTracingTest
 import ai.dev.kit.tracing.LITELLM_URL
 import ai.dev.kit.tracing.autologging.createLiteLLMClient
+import com.anthropic.client.AnthropicClient
+import com.anthropic.client.okhttp.AnthropicOkHttpClient
+import com.anthropic.models.messages.Model
+import com.openai.client.OpenAIClient
+import com.openai.client.okhttp.OpenAIOkHttpClient
 import com.openai.models.ChatModel
 import com.openai.models.chat.completions.ChatCompletionCreateParams
 import io.opentelemetry.api.common.AttributeKey
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
+import java.time.Duration
 import kotlin.jvm.optionals.getOrNull
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
@@ -45,6 +51,38 @@ class OpenAITracingTest() : BaseOpenTelemetryTracingTest() {
     }
 
     @Test
+    fun `test Anthropic auto tracing`() = runTest {
+        val client = createAnthropicClient()
+
+        val params = com.anthropic.models.completions.CompletionCreateParams.builder()
+            .prompt("Generate polite greeting and introduce yourself")
+            .model(Model.CLAUDE_3_5_HAIKU_20241022).temperature(1.1)
+            .maxTokensToSample(8000L)
+            .build()
+        client.completions().create(params)
+
+        val traces = analyzeSpans()
+        println("TRACES: $traces")
+
+        /*
+        assertEquals(1, traces.size)
+        val trace = traces.firstOrNull()
+        assertNotNull(trace)
+
+        assertEquals(
+            LITELLM_URL,
+            trace.attributes[AttributeKey.stringKey("gen_ai.openai.api_base")]
+        )
+        assertTrue(
+            trace.attributes[AttributeKey.stringKey("gen_ai.response.model")]?.startsWith(ChatModel.Companion.GPT_4O_MINI.asString()) ?: false
+        )
+        val content = trace.attributes[AttributeKey.stringKey("gen_ai.completion.0.content")]
+        assertNotNull(content)
+        assertTrue(content.isNotEmpty())
+        */
+    }
+
+    @Test
     fun `test OpenAI auto tracing when instrumentation is off`() = runTest {
         val client = createLiteLLMClient()
         val params = ChatCompletionCreateParams.Companion.builder()
@@ -61,5 +99,13 @@ class OpenAITracingTest() : BaseOpenTelemetryTracingTest() {
         val content = result.choices().first().message().content().getOrNull()
         assertNotNull(content)
         assertTrue(content.isNotEmpty())
+    }
+
+    private fun createAnthropicClient(): AnthropicClient {
+        return AnthropicOkHttpClient.builder()
+            .baseUrl(LITELLM_URL)
+            .apiKey(System.getenv("LITELLM_API_KEY") ?: error("LITELLM_API_KEY environment variable is not set"))
+            .timeout(Duration.ofSeconds(60))
+            .build()
     }
 }
