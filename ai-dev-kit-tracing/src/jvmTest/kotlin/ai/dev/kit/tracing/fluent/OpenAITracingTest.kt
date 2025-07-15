8cca51e781e53dfbdab585bed12ec59208ab1032
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2025-07-15 12:14:42 +0200

    feat(anthropic): add assertions in test case

--- ai-dev-kit-tracing/src/jvmTest/kotlin/ai/dev/kit/tracing/fluent/OpenAITracingTest.kt
+++ ai-dev-kit-tracing/src/jvmTest/kotlin/ai/dev/kit/tracing/fluent/OpenAITracingTest.kt
@@ -59,43 +59,34 @@ class OpenAITracingTest() : BaseOpenTelemetryTracingTest() {
             .maxTokens(1000L)
             .temperature(0.8)
             .addUserMessage("Generate polite greeting and introduce yourself")
-            .addAssistantMessage("Hi! I'm Claude")
-            .addUserMessage("What is your name?")
-            .model(Model.CLAUDE_3_7_SONNET_LATEST)
+            .model(Model.CLAUDE_3_5_SONNET_20240620)
             .build()
 
         val message = client.messages().create(params)
         client.completions()
         println("MESSAGE: $message")
 
-        /*
-        val params = com.anthropic.models.completions.CompletionCreateParams.builder()
-            .prompt("Generate polite greeting and introduce yourself")
-            .model(Model.CLAUDE_3_5_SONNET_20240620).temperature(1.1)
-            .maxTokensToSample(1000L)
-            .build()
-        client.completions().create(params)
-        */
-
         val traces = analyzeSpans()
         println("TRACES: $traces")
 
-        /*
         assertEquals(1, traces.size)
         val trace = traces.firstOrNull()
         assertNotNull(trace)
 
         assertEquals(
             LITELLM_URL,
-            trace.attributes[AttributeKey.stringKey("gen_ai.openai.api_base")]
+            trace.attributes[AttributeKey.stringKey("gen_ai.anthropic.api_base")]
         )
-        assertTrue(
-            trace.attributes[AttributeKey.stringKey("gen_ai.response.model")]?.startsWith(ChatModel.Companion.GPT_4O_MINI.asString()) ?: false
-        )
-        val content = trace.attributes[AttributeKey.stringKey("gen_ai.completion.0.content")]
-        assertNotNull(content)
-        assertTrue(content.isNotEmpty())
-        */
+
+        assertTrue(trace.attributes[AttributeKey.stringKey("gen_ai.response.model")]?.startsWith(Model.CLAUDE_3_5_SONNET_20240620.asString()) == true)
+
+        val type = trace.attributes[AttributeKey.stringKey("gen_ai.completion.0.type")]
+        assertNotNull(type)
+        assertTrue(type.isNotEmpty())
+
+        val text = trace.attributes[AttributeKey.stringKey("gen_ai.completion.0.text")]
+        assertNotNull(text)
+        assertTrue(text.isNotEmpty())
     }
 
     @Test
