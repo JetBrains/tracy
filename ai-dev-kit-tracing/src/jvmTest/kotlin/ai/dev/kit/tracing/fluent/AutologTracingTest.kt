d89fcf5c05e9d9a347c550035fe1853477b9986f
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2025-07-24 22:29:18 +0200

    feat(gemini): test 2 failing scenarios (timeout, 400 bad request)

--- ai-dev-kit-tracing/src/jvmTest/kotlin/ai/dev/kit/tracing/fluent/AutologTracingTest.kt
+++ ai-dev-kit-tracing/src/jvmTest/kotlin/ai/dev/kit/tracing/fluent/AutologTracingTest.kt
@@ -2,10 +2,10 @@ package ai.dev.kit.tracing.fluent
 
 import ai.dev.kit.instrument
 import ai.dev.kit.tracing.BaseOpenTelemetryTracingTest
+import ai.dev.kit.tracing.autologging.createGeminiClient
 import ai.dev.kit.tracing.autologging.createLiteLLMClient
-import com.google.genai.types.ListModelsConfig
-import com.openai.client.OpenAIClient
-import com.openai.client.okhttp.OpenAIOkHttpClient
+import com.google.genai.errors.ClientException
+import com.google.genai.errors.GenAiIOException
 import com.openai.models.ChatModel
 import com.openai.models.chat.completions.ChatCompletionCreateParams
 import io.opentelemetry.api.common.AttributeKey
@@ -13,14 +13,17 @@ import io.opentelemetry.api.trace.StatusCode
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
-import java.time.Duration
 import kotlin.jvm.optionals.getOrNull
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.ResponseBody.Companion.toResponseBody
+import java.net.SocketTimeoutException
 import com.google.genai.Client as GeminiClient
 import com.google.genai.types.GenerateContentConfig as GeminiGenerateContentConfig
-import com.google.genai.types.HttpOptions as GeminiHttpOptions
 
 const val LITELLM_URL = "https://litellm.labs.jb.gg"
 
@@ -53,38 +56,6 @@ class AutologTracingTest() : BaseOpenTelemetryTracingTest() {
     }
 
     @Test
-    fun `test Gemini auto tracing`() = runTest {
-        val model = "gemini-1.5-pro"
-        val client = instrument(createGeminiClient())
-
-        client.models.generateContent(
-            model,
-            "Generate polite greeting and introduce yourself",
-            GeminiGenerateContentConfig.builder()
-                .temperature(0.8f)
-                .build()
-        )
-
-        val traces = analyzeSpans()
-
-        assertEquals(1, traces.size)
-        val trace = traces.firstOrNull()
-        assertNotNull(trace)
-
-        assertTrue(trace.status.statusCode == StatusCode.OK)
-        assertEquals(
-            LITELLM_URL,
-            trace.attributes[AttributeKey.stringKey("gen_ai.gemini.api_base")]
-        )
-        assertTrue(
-            trace.attributes[AttributeKey.stringKey("gen_ai.response.model")]?.startsWith(model) == true
-        )
-        val text = trace.attributes[AttributeKey.stringKey("gen_ai.completion.0.content")]
-        assertNotNull(text)
-        assertTrue(text.isNotEmpty())
-    }
-
-    @Test
     fun `test OpenAI auto tracing when instrumentation is off`() = runTest {
         val client = createLiteLLMClient()
         val params = ChatCompletionCreateParams.Companion.builder()
@@ -103,15 +74,140 @@ class AutologTracingTest() : BaseOpenTelemetryTracingTest() {
         assertTrue(content.isNotEmpty())
     }
 
-    private fun createGeminiClient(): GeminiClient {
-        return GeminiClient.builder()
-            .apiKey(System.getenv("LITELLM_API_KEY") ?: error("LITELLM_API_KEY environment variable is not set"))
-            .httpOptions(
-                GeminiHttpOptions.builder()
-                    .baseUrl("$LITELLM_URL/gemini")
-                    .timeout(Duration.ofSeconds(60).toMillis().toInt())
+    @Test
+    fun `test Gemini auto tracing`() = runTest {
+        val model = "gemini-1.5-pro"
+        val client = instrument(createGeminiClient())
+
+        client.models.generateContent(
+            model,
+            "Generate polite greeting and introduce yourself",
+            GeminiGenerateContentConfig.builder()
+                .temperature(0.8f)
+                .build()
+        )
+
+        val traces = analyzeSpans()
+
+        assertEquals(1, traces.size)
+        val trace = traces.firstOrNull()
+        assertNotNull(trace)
+
+        assertEquals(StatusCode.OK, trace.status.statusCode)
+        assertEquals(
+            LITELLM_URL,
+            trace.attributes[AttributeKey.stringKey("gen_ai.gemini.api_base")]
+        )
+        assertTrue(
+            trace.attributes[AttributeKey.stringKey("gen_ai.response.model")]?.startsWith(model) == true
+        )
+        val text = trace.attributes[AttributeKey.stringKey("gen_ai.completion.0.content")]
+        assertNotNull(text)
+        assertTrue(text.isNotEmpty())
+    }
+
+    @Test
+    fun `test Gemini span error code when timeout occurs`() {
+        val client = instrument(createGeminiClient())
+
+        val timeoutInterceptor = object : Interceptor {
+            override fun intercept(chain: Interceptor.Chain): Response {
+                throw SocketTimeoutException("My Custom Timeout")
+            }
+        }
+        // the installed interceptor will imitate timeout
+        installHttpInterceptor(client, interceptor = timeoutInterceptor)
+
+        try {
+            client.models.generateContent(
+                "gemini-1.5-pro",
+                "Generate polite greeting and introduce yourself",
+                GeminiGenerateContentConfig.builder()
+                    .temperature(0.8f)
                     .build()
             )
+        } catch (_: GenAiIOException) {
+            // suppress
+        }
+
+        val traces = analyzeSpans()
+
+        assertEquals(1, traces.size)
+        val trace = traces.firstOrNull()
+        assertNotNull(trace)
+
+        assertEquals(StatusCode.ERROR, trace.status.statusCode)
+        assertEquals(
+            LITELLM_URL,
+            trace.attributes[AttributeKey.stringKey("gen_ai.gemini.api_base")]
+        )
+
+        val event = trace.events.firstOrNull { it.name == "exception" }
+        assertNotNull(event)
+        assertEquals(
+            "My Custom Timeout",
+            event.attributes[AttributeKey.stringKey("exception.message")],
+        )
+    }
+
+    @Test
+    fun `test Gemini span error code when returning 400 response code`() {
+        val client = instrument(createGeminiClient())
+
+        val errorResponseCodeInterceptor = object : Interceptor {
+            override fun intercept(chain: Interceptor.Chain): Response {
+                val response = chain.proceed(chain.request())
+                val newBody = "{ \"invalid\": \"json\" }"
+                    .toResponseBody(response.body?.contentType())
+
+                return response.newBuilder()
+                    .body(newBody)
+                    .code(400)
+                    .build()
+            }
+        }
+
+        // the installed interceptor will imitate a failed request
+        installHttpInterceptor(client, interceptor = errorResponseCodeInterceptor)
+
+        try {
+            client.models.generateContent(
+                "gemini-1.5-pro",
+                "Generate polite greeting and introduce yourself",
+                GeminiGenerateContentConfig.builder()
+                    .temperature(0.8f)
+                    .build()
+            )
+        } catch (_: ClientException) {
+            // suppress
+        }
+
+        val traces = analyzeSpans()
+
+        assertEquals(1, traces.size)
+        val trace = traces.firstOrNull()
+        assertNotNull(trace)
+
+        assertEquals(StatusCode.ERROR, trace.status.statusCode)
+        assertEquals(
+            LITELLM_URL,
+            trace.attributes[AttributeKey.stringKey("gen_ai.gemini.api_base")]
+        )
+    }
+
+    private fun installHttpInterceptor(client: GeminiClient, interceptor: Interceptor) {
+        val apiClientField = GeminiClient::class.java.getDeclaredField("apiClient")
+            .apply { isAccessible = true }
+        val apiClient = apiClientField.get(client)
+
+        val httpClientField = apiClient.javaClass.superclass.getDeclaredField("httpClient")
+            .apply { isAccessible = true }
+        val originalHttpClient = httpClientField.get(apiClient) as OkHttpClient
+
+        val modifiedHttpClient = originalHttpClient.newBuilder()
+            .addInterceptor(interceptor)
             .build()
+
+        httpClientField.set(apiClient, modifiedHttpClient)
     }
 }
