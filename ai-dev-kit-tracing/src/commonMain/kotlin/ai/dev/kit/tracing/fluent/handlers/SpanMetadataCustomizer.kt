cbfac56e17d552b526151f6f204d74f908956d4f
Viacheslav Suvorov <suvslavka@gmail.com>
2025-09-08 14:38:51 +0200

    [JBAI-16902] Add ability to change span name in span methadata customizer

---
+++ ai-dev-kit-tracing/src/commonMain/kotlin/ai/dev/kit/tracing/fluent/handlers/SpanMetadataCustomizer.kt
@@ -0,0 +1,28 @@
+package ai.dev.kit.tracing.fluent.handlers
+
+/**
+ * Defines a contract for customizing how trace span names and attributes
+ * (input arguments and outputs) are generated during instrumented method calls.
+ *
+ * Use this interface to:
+ * - Override the default span name resolution logic.
+ *   The resolution pipeline is:
+ *   1. If [resolveSpanName] returns a non-null value, it is used.
+ *   2. Otherwise, the annotation name is used.
+ *   3. If still undefined, the method name is used.
+ *   See also [ai.dev.kit.tracing.fluent.processor.createSpan].
+ * - Transform input arguments into structured string representations.
+ * - Convert method returns values into structured string representations.
+ *
+ * ### Base Implementation
+ * - [DefaultSpanMetadataCustomizer]: A default implementation that serializes input arguments
+ *   and outputs into JSON-formatted strings.
+ */
+
+interface SpanMetadataCustomizer {
+    fun resolveSpanName(method: PlatformMethod, args: Array<Any?>): String? = null
+    fun formatInputAttributes(method: PlatformMethod, args: Array<Any?>): String
+    fun formatOutputAttribute(result: Any?): String = result.toString()
+}
+
+expect class PlatformMethod
