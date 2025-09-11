1c0268983b02a20b07440a17032478d7a29c56ce
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2025-09-11 12:55:30 +0200

    feat: compare actual response with traced data in anthropic test case

--- ai-dev-kit-tracing/src/jvmTest/kotlin/ai/dev/kit/tracing/fluent/HttpClientTracingTest.kt
+++ ai-dev-kit-tracing/src/jvmTest/kotlin/ai/dev/kit/tracing/fluent/HttpClientTracingTest.kt
@@ -13,6 +13,11 @@ import io.ktor.utils.io.*
 import io.opentelemetry.api.common.AttributeKey
 import io.opentelemetry.api.trace.StatusCode
 import kotlinx.coroutines.test.runTest
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.int
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
@@ -22,10 +27,19 @@ import kotlin.test.assertTrue
 
 @Tag("SkipForNonLocal")
 class HttpClientTracingTest : BaseOpenTelemetryTracingTest() {
+    private fun String.unquote(): String {
+        if (this.startsWith("\"") && this.endsWith("\"")) {
+            return this.substring(1, this.length - 1)
+        }
+        return this
+    }
+
     @Test
     fun `test Ktor HttpClient auto tracing for Anthropic`() = runTest {
         val client: HttpClient = instrument(HttpClient(), provider = HttpClientLLMProvider.Anthropic)
         val model = "claude-sonnet-4-20250514"
+        val promptMessage = "Hello, world!"
+
         val response = client.post("$LITELLM_URL/v1/messages") {
             val apiKey = System.getenv("LITELLM_API_KEY") ?: error("LITELLM_API_KEY environment variable is not set")
 
@@ -36,7 +50,7 @@ class HttpClientTracingTest : BaseOpenTelemetryTracingTest() {
                     "max_tokens": 1024,
                     "messages": [
                         {
-                            "content": "Hello, world!",
+                            "content": "$promptMessage",
                             "role": "user"
                         }
                     ],
@@ -47,31 +61,52 @@ class HttpClientTracingTest : BaseOpenTelemetryTracingTest() {
 
         val traces = analyzeSpans()
 
+        // assert expectations on a trace
         assertEquals(1, traces.size)
         val trace = traces.firstOrNull()
         assertNotNull(trace)
 
         assertEquals(StatusCode.OK, trace.status.statusCode)
 
-        assertEquals(
-            LITELLM_URL,
-            trace.attributes[AttributeKey.stringKey("gen_ai.api_base")]
-        )
+        assertEquals("anthropic", trace.attributes[AttributeKey.stringKey("gen_ai.system")])
+        assertEquals(LITELLM_URL, trace.attributes[AttributeKey.stringKey("gen_ai.api_base")])
 
-        assertTrue(
-            trace.attributes[AttributeKey.stringKey("gen_ai.response.model")]?.startsWith(model) == true
-        )
+        val tracedModel = trace.attributes[AttributeKey.stringKey("gen_ai.response.model")]
+        assertEquals(true, tracedModel?.startsWith(model))
 
-        val type = trace.attributes[AttributeKey.stringKey("gen_ai.completion.0.type")]
-        assertNotNull(type)
-        assertTrue(type.isNotEmpty())
+        assertEquals(promptMessage, trace.attributes[AttributeKey.stringKey("gen_ai.prompt.0.content")]?.unquote())
+        assertEquals("user", trace.attributes[AttributeKey.stringKey("gen_ai.prompt.0.role")])
 
-        val text = trace.attributes[AttributeKey.stringKey("gen_ai.completion.0.content")]
-        assertNotNull(text)
-        assertTrue(text.isNotEmpty())
+        val completionType = trace.attributes[AttributeKey.stringKey("gen_ai.completion.0.type")]
+        val completionText = trace.attributes[AttributeKey.stringKey("gen_ai.completion.0.content")]
+
+        assertNotNull(completionType)
+        assertTrue(completionType.isNotEmpty())
+
+        assertNotNull(completionText)
+        assertTrue(completionText.isNotEmpty())
+
 
         // assert that tracing doesn't consume the response body
-        assertTrue(response.bodyAsText().isNotEmpty())
+        val responseBody = response.bodyAsText()
+        assertTrue(responseBody.isNotEmpty())
+
+        // compare trace with the actual response
+        val responseJson = Json.parseToJsonElement(responseBody).jsonObject
+
+        assertEquals(responseJson["model"]!!.jsonPrimitive.content, tracedModel)
+        assertEquals(responseJson["content"]!!.jsonArray[0].jsonObject["type"]!!.jsonPrimitive.content, completionType)
+        assertEquals(responseJson["content"]!!.jsonArray[0].jsonObject["text"]!!.jsonPrimitive.content, completionText.unquote())
+
+        val usage = responseJson["usage"]!!.jsonObject
+        assertEquals(
+            usage["input_tokens"]!!.jsonPrimitive.int,
+            trace.attributes[AttributeKey.longKey("gen_ai.usage.input_tokens")]!!.toInt()
+        )
+        assertEquals(
+            usage["output_tokens"]!!.jsonPrimitive.int,
+            trace.attributes[AttributeKey.longKey("gen_ai.usage.output_tokens")]!!.toInt()
+        )
     }
 
     @Test
@@ -116,6 +151,8 @@ class HttpClientTracingTest : BaseOpenTelemetryTracingTest() {
         // assert that tracing doesn't consume the response body
         assertTrue(response.bodyAsText().isNotEmpty())
     }
+
+    // TODO: write test where a serializable object is set as a request body
 
     @Test
     fun `test Ktor HttpClient auto tracing for Bad Request in OpenAI`() = runTest {
