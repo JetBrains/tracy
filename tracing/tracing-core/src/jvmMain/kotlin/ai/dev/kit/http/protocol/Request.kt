3afe61b631056715d78f41dbfc7a794e78e82ed7
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2025-11-19 13:31:38 +0100

    feat: add docs to request and response data classes

--- tracing/tracing-core/src/jvmMain/kotlin/ai/dev/kit/http/protocol/Request.kt
+++ tracing/tracing-core/src/jvmMain/kotlin/ai/dev/kit/http/protocol/Request.kt
@@ -7,11 +7,35 @@ import okhttp3.HttpUrl
 import okhttp3.MediaType
 
 
+/**
+ * Represents an HTTP request with its associated properties.
+ *
+ * @param url The URL to which the request is sent.
+ *            This includes the scheme, host, and path segments.
+ * @param contentType The content type of the request, indicating the type of data included in the body.
+ *                    This may be null if not specified.
+ * @param body The body of the request, containing the actual data to be sent.
+ *             This can be represented as JSON or form data.
+ */
 data class Request(
     val url: Url,
     val contentType: ContentType?,
     val body: RequestBody,
 )
+
+/**
+ * Represents the body content of an HTTP request. It can either be a JSON payload or form data.
+ *
+ * This sealed class is used as part of the [Request] data structure to encapsulate the various
+ * types of data that can be transmitted as the body of an HTTP request.
+ *
+ * - [Json]: Represents a JSON body containing structured data.
+ * - [DataForm]: Represents form-data typically used in multipart requests.
+ */
+sealed class RequestBody {
+    data class Json(val json: JsonElement) : RequestBody()
+    data class DataForm(val data: FormData) : RequestBody()
+}
 
 data class Url(
     val scheme: String,
@@ -22,11 +46,6 @@ data class Url(
 fun HttpUrl.toRequestUrl() = Url(scheme, host, pathSegments)
 
 fun MediaType.toContentType(): ContentType = ContentType.parse(this.toString())
-
-sealed class RequestBody {
-    data class Json(val json: JsonElement) : RequestBody()
-    data class DataForm(val data: FormData) : RequestBody()
-}
 
 fun RequestBody.asJson(): JsonElement? {
     return when (this) {
