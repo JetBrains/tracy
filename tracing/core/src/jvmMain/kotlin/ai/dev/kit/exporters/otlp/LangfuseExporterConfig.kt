957dcd34f7bc271aee4e89e3a1560f7fdf6f6624
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2025-12-11 16:24:38 +0100

    refactor: use `FilteredSpanData` on all spans

--- tracing/core/src/jvmMain/kotlin/ai/dev/kit/exporters/otlp/LangfuseExporterConfig.kt
+++ tracing/core/src/jvmMain/kotlin/ai/dev/kit/exporters/otlp/LangfuseExporterConfig.kt
@@ -260,16 +260,7 @@ class MediaContentAttributeFilteringSpanExporter(
     private val delegate: SpanExporter
 ) : SpanExporter {
     override fun export(spans: Collection<SpanData>): CompletableResultCode {
-        val prefix = UploadableMediaContentAttributeKeys.KEY_NAME_PREFIX
-        val filteredSpans = spans.map { span ->
-            // when a span indeed contains attributes that should be filtered out,
-            // we apply the filtering; otherwise, the span remains unmodified
-            if (span.attributes.asMap().keys.any { it.key.startsWith(prefix) }) {
-                FilteredSpanData(span)
-            } else {
-                span
-            }
-        }
+        val filteredSpans = spans.map { FilteredSpanData(it) }
         return delegate.export(filteredSpans)
     }
 
