72fdff2acd63b25ff01cc140379a9b6cd739d55f
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2026-01-27 17:48:23 +0100

    feat: turn `test-utils` into Java test fixtures

--- tracing/test-utils/build.gradle.kts
+++ tracing/test-utils/build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
     kotlin("multiplatform")
     alias(libs.plugins.kotlin.serialization)
     id("ai.kotlin.dokka")
+    `java-test-fixtures`
 }
 
 kotlin {
@@ -16,33 +17,50 @@ kotlin {
 
     jvm {
         compilerOptions.jvmTarget = JVM_17
+
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
     }
 
     sourceSets {
         commonMain {
             dependencies {
-                implementation(project(":tracing:core"))
-                implementation(libs.kotlinx.serialization.core)
-                implementation(libs.kotlinx.serialization.json)
+//                implementation(project(":tracing:core"))
+//                implementation(libs.kotlinx.serialization.core)
+//                implementation(libs.kotlinx.serialization.json)
             }
         }
 
         jvmMain {
             dependencies {
-                implementation(libs.gemini)
-                implementation(libs.okhttp)
-                implementation(libs.opentelemetry)
-                implementation(libs.opentelemetry.kotlin)
-                implementation(libs.opentelemetry.sdk)
+//                implementation(libs.gemini)
+//                implementation(libs.okhttp)
+//                implementation(libs.opentelemetry)
+//                implementation(libs.opentelemetry.kotlin)
+//                implementation(libs.opentelemetry.sdk)
+//                implementation(libs.opentelemetry.sdk.testing)
+//                implementation(libs.opentelemetry.semconv.incubating)
+//                implementation(libs.opentelemetry.exporter.otlp)
+//                implementation(libs.opentelemetry.exporter.logging)
+//                implementation(libs.kotlin.test)
+//                implementation(libs.junit)
+//                implementation(libs.junit.params)
+//                implementation(libs.kotlinx.coroutines)
+//                implementation(libs.ktor.client)
+            }
+        }
+
+        val jvmTestFixtures by getting {
+            dependencies {
+                // TODO: needed explicitly?
+                // implementation(kotlin("stdlib"))
+
+                implementation(project(":tracing:core"))
                 implementation(libs.opentelemetry.sdk.testing)
-                implementation(libs.opentelemetry.semconv.incubating)
-                implementation(libs.opentelemetry.exporter.otlp)
-                implementation(libs.opentelemetry.exporter.logging)
+                implementation(libs.ktor.client)
                 implementation(libs.kotlin.test)
                 implementation(libs.junit)
-                implementation(libs.junit.params)
-                implementation(libs.kotlinx.coroutines)
-                implementation(libs.ktor.client)
             }
         }
     }
