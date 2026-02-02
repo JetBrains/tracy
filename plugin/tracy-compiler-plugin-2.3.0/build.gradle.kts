5b4601cf14dbdf0a5d25ea20141a97fc6428f82c
Viacheslav Suvorov <suvslavka@gmail.com>
2026-02-02 14:32:26 +0100

    TRACY-11 Support 2.3.0 version of Kotlin in Tracy

---
+++ plugin/tracy-compiler-plugin-2.3.0/build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+
+plugins {
+    id("ai.jetbrains.tracy.published-artifact")
+    id("org.jetbrains.kotlin.multiplatform") version "2.3.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvmToolchain(17)
+    jvm {
+        compilerOptions.jvmTarget = JVM_17
+    }
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                compileOnly("org.jetbrains.kotlin:kotlin-compiler-embeddable")
+            }
+        }
+    }
+}
+
+publishing {
+    publications.withType<MavenPublication>().configureEach {
+        pom {
+            name.set(artifactId)
+            description.set("Kotlin compiler plugin for enabling Tracy tracing annotations with Kotlin 2.3.0.")
+        }
+    }
+}
