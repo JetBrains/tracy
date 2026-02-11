5fb574ccd0d04142b40dbc4d6058f72d13637b8f
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2026-02-11 18:35:39 +0100

    feat: move comment into KDoc

--- tracing/openai/src/jvmMain/kotlin/ai/jetbrains/tracy/openai/clients/OpenAIClient.kt
+++ tracing/openai/src/jvmMain/kotlin/ai/jetbrains/tracy/openai/clients/OpenAIClient.kt
@@ -16,12 +16,14 @@ import com.openai.client.OpenAIClient
  * All LLM API calls made using this client will be automatically traced,
  * capturing request/response attributes as span data.
  *
+ * To patch the given client, the instrumentation
+ * adds an interceptor into the client in-place.
+ *
  * @param client The [OpenAIClient] instance to instrument.
  *
  * @see OpenAILLMTracingAdapter
  */
 fun instrument(client: OpenAIClient) {
-    // adds an interceptor into the client in-place
     patchOpenAICompatibleClient(
         client = client,
         interceptor = OpenTelemetryOkHttpInterceptor(adapter = OpenAILLMTracingAdapter())
