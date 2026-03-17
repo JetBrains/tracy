1356aa7c10a08c29a814a316907f602e2aff47d0
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2026-03-17 21:36:02 +0100

    feat(test-utils): turn `mockwebserver` into `api` dependency

--- tracing/test-utils/build.gradle.kts
+++ tracing/test-utils/build.gradle.kts
@@ -23,7 +23,7 @@ kotlin {
             dependencies {
                 implementation(project(":tracing:core"))
                 implementation(libs.opentelemetry.sdk.testing)
-                implementation(libs.okhttp.mockwebserver)
+                api(libs.okhttp.mockwebserver)
                 implementation(libs.ktor.client)
                 implementation(libs.kotlin.test)
                 implementation(libs.junit)
